8baa00a7172789e7ca8723edf176a31801480a12
yeonsoo <leeyeon990127@gmail.com>
2026-06-11 10:50:08 +0900

    첫번째 커밋

---
+++ src/d1_p27/FirstClass.java
@@ -0,0 +1,11 @@
+package d1_p27;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+//        System.out.println("이 문장이 실행 될까요?");
+
+        /*System.out.println("그럼 이 문장은요?");*/
+        }
+}
