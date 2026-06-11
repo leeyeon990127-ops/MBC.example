c52f431d6416d304d0678dea12489298639b9366
yeonsoo <leeyeon990127@gmail.com>
2026-06-11 13:12:21 +0900

    6/11 오전 java 개념 예제코드 작성(p49~61)

---
+++ src/d1_p49/CharExample.java
@@ -0,0 +1,11 @@
+package d1_p49;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = 65;   //아스키코드값
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
---
+++ src/d1_p53/LongExample.java
@@ -0,0 +1,15 @@
+package d1_p53;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 200L;
+//        long var3 = 100000000000;
+        long var4 = 100000000000L;
+
+        System.out.println(var1);
+        System.out.println(var2);
+//        System.out.println(var3);
+        System.out.println(var4);
+    }
+}
