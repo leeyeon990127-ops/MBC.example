f9785f704c9cc6e11d0ca85f2ba7ef6f10b84cfd
yeonsoo <leeyeon990127@gmail.com>
2026-06-12 18:29:24 +0900

    6/12 오후 java 기본 개념 완료 예제코드 작성(p.16 ~ 27) if, switch, for while do-while

---
+++ src/d2_p18/AdvForExample.java
@@ -0,0 +1,21 @@
+package d2_p18;
+
+public class AdvForExample {
+    public static void main(String[] args) {
+        int[] scores = {9, 7, 8, 9, 8};
+
+        // 향상된 for 문
+        int sum = 0;
+        for(int s : scores) {
+            sum += s;
+        }
+        System.out.println("sum : " + sum);
+
+        // 일반 for문
+        int sum2 = 0;
+        for(int i=0; i<scores.length; i++){
+            sum2 += scores[i];
+        }
+        System.out.println("sum2 : " + sum2);
+    }
+}
---
+++ src/d2_p25/BreakExample.java
@@ -0,0 +1,27 @@
+package d2_p25;
+
+public class BreakExample {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (true) {
+            System.out.println(i);
+
+            if (i == 6)
+                break;
+
+            i = i + 1;
+        }
+
+        // for 문 무한루프
+        int j = 10;
+        for(; ; ) {
+            System.out.println(j);
+
+            if (j == 15)
+                break;
+            j += 1;
+        }
+    }
+}
